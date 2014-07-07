1116d1da6435f584ccc92685a4a38b2a512b25b0
Alexis von Glasow <vonglasow@gmail.com>
2014-07-07 22:01:21 +0200

    change code old atoum is used for praspel

--- tests/units/Conway.php
+++ tests/units/Conway.php
@@ -3,26 +3,22 @@
 namespace GameOfLife\tests\units;
 
 use mageekguy\atoum;
+use GameOfLife\Conway as game;
 
 class Conway extends atoum\test
 {
-    public function testClass()
-    {
-        $this->object(new \GameOfLife\Conway)->isInstanceOf('\GameOfLife\Conway');
-    }
-
     public function testInitEmptyWorld()
     {
-        $this->object($this->newTestedInstance)->isInstanceOf('\GameOfLife\Conway')
-            ->if()->array($this->testedInstance->getWorld())->isEmpty()
-                ->object($this->testedInstance->initEmptyWorld())->isInstanceOf('\GameOfLife\Conway')
-            ->then()->array($this->testedInstance->getWorld())->isNotEmpty()
+        $this->object($conway = new game)->isInstanceOf('\GameOfLife\Conway')
+            ->if()->array($conway->getWorld())->isEmpty()
+                ->object($conway->initEmptyWorld())->isInstanceOf('\GameOfLife\Conway')
+            ->then()->array($conway->getWorld())->isNotEmpty()
             ->and()
-                ->object($this->testedInstance->setX(3))->isInstanceOf('\GameOfLife\Conway')
-                ->object($this->testedInstance->setY(3))->isInstanceOf('\GameOfLife\Conway')
+                ->object($conway->setX(3))->isInstanceOf('\GameOfLife\Conway')
+                ->object($conway->setY(3))->isInstanceOf('\GameOfLife\Conway')
             ->if()
-                ->object($this->testedInstance->initEmptyWorld())->isInstanceOf('\GameOfLife\Conway')
-            ->then()->array($this->testedInstance->getWorld())->isEqualTo(
+                ->object($conway->initEmptyWorld())->isInstanceOf('\GameOfLife\Conway')
+            ->then()->array($conway->getWorld())->isEqualTo(
                 [
                     [0, 0, 0],
                     [0, 0, 0],
@@ -34,8 +30,8 @@ class Conway extends atoum\test
 
     public function testComputeNewState()
     {
-        $this->object($this->newTestedInstance)->isInstanceOf('\GameOfLife\Conway')
-            ->if()->object($this->testedInstance->setWorld(
+        $this->object($conway = new game)->isInstanceOf('\GameOfLife\Conway')
+            ->if()->object($conway->setWorld(
                 [
                     [0, 0, 0],
                     [0, 0, 0],
@@ -43,11 +39,11 @@ class Conway extends atoum\test
                 ]
             ))->isInstanceOf('\GameOfLife\Conway')
             ->and()
-                ->object($this->testedInstance->setX(3))->isInstanceOf('\GameOfLife\Conway')
-                ->object($this->testedInstance->setY(3))->isInstanceOf('\GameOfLife\Conway')
+                ->object($conway->setX(3))->isInstanceOf('\GameOfLife\Conway')
+                ->object($conway->setY(3))->isInstanceOf('\GameOfLife\Conway')
             ->if()
-                ->object($this->testedInstance->computeNewState())->isInstanceOf('\GameOfLife\Conway')
-            ->then()->array($this->testedInstance->getWorld())->isEqualTo(
+                ->object($conway->computeNewState())->isInstanceOf('\GameOfLife\Conway')
+            ->then()->array($conway->getWorld())->isEqualTo(
                 [
                     [0, 0, 0],
                     [0, 0, 0],
