8887a8d4887bd247b4c4c079cf6cb37df2bbb154
Alexis von Glasow <vonglasow@gmail.com>
2014-07-15 16:04:28 +0200

    Add code coverage

--- .atoum.php
+++ .atoum.php
@@ -2,3 +2,10 @@
 $runner->setBootstrapFile(__DIR__ . '/.bootstrap.atoum.php')
     ->addExtension(new Atoum\PraspelExtension\Manifest())
 ;
+
+$coverageField = new atoum\report\fields\runner\coverage\html('Game Of Life', __DIR__ . '/tests/coverage/');
+$coverageField->setRootUrl('http://127.0.0.1');
+
+$report = $script->addDefaultReport();
+$report->addField($coverageField)
+       ->addField(new atoum\report\fields\runner\result\logo());
