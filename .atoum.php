f42c02e44636652dede4e3017cb9d73b8a616e7f
Alexis von Glasow <vonglasow@gmail.com>
2014-12-01 21:22:12 +0100

    Add tests folders to atoum config and upgrade dependencies

--- .atoum.php
+++ .atoum.php
@@ -1,6 +1,7 @@
 <?php
 $runner->setBootstrapFile(__DIR__ . '/.bootstrap.atoum.php')
     ->addExtension(new Atoum\PraspelExtension\Manifest())
+    ->addTestsFromDirectory('tests/units');
 ;
 
 $coverageField = new atoum\report\fields\runner\coverage\html('Game Of Life', __DIR__ . '/tests/coverage/');
@@ -9,3 +10,4 @@ $coverageField->setRootUrl('http://127.0.0.1');
 $report = $script->addDefaultReport();
 $report->addField($coverageField)
        ->addField(new atoum\report\fields\runner\result\logo());
+
